ec0fc0661caca833b8bad12620b2780ac4169c3d
Ayakura Yuki <AyakuraYuki@users.noreply.github.com>
2018-01-23 23:45:30 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # URLDecoder_Swift
-一个用于macOS的URLDecoder，使用Swift编写
+
+> 一个用于macOS的URLDecoder，使用Swift编写
+
+## 简介
+
+本项目为Swift编程学习以及macOS Application开发入门项目
+
+本项目灵感来源于URL编码和解码，为方便进行URL编码和解码而开发（主要原因是打开几个在线编码/解码用的时间比较久）
+
+## 环境
+
+* macOS 10.13.2
+* Xcode 9.2 (9C40b)
+* 
---
+++ URLDecoderSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  URLDecoderSwift
+//
+//  Created by 绫仓优希 on 2018/1/23.
+//  Copyright © 2018年 YukiAyakura. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override var representedObject: Any? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+    
+}
