32233a205cad403aa0c988a1eb0fe4b78c6d32a9
Ayakura Yuki <AyakuraYuki@users.noreply.github.com>
2018-02-01 16:06:49 +0800

    AppIcon and base controller

--- URLDecoderSwift/ViewController.swift
+++ URLDecoderSwift/ViewController.swift
@@ -10,13 +10,34 @@ import Cocoa
 
 class ViewController: NSViewController {
     
+    var welcomeFlag: Bool = true
+    
+    /// 原文输入框
+    @IBOutlet weak var plainText: NSTextFieldCell!
+    /// 编码内容输入框
+    @IBOutlet weak var cipherText: NSTextFieldCell!
+    /// 编码按钮
+    @IBOutlet weak var encodeButton: NSButton!
+    /// 解码按钮
+    @IBOutlet weak var decodeButton: NSButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let alert = NSAlert()
-        alert.messageText = NSLocalizedString("Welcome", comment: "Welcome")
-        alert.alertStyle = NSAlert.Style.informational
-        alert.runModal()
         // Do any additional setup after loading the view.
+        plainText.placeholderString = NSLocalizedString("plainTextHint", comment: "Placeholder of plain text field cell")
+        cipherText.placeholderString = NSLocalizedString("cipherTextHint", comment: "Placeholder of cipher text field cell")
+        encodeButton.title = NSLocalizedString("encodeButtonTitle", comment: "Title of encode button")
+        decodeButton.title = NSLocalizedString("decodeButtonTitle", comment: "Title of decode button")
+    }
+    
+    override func viewDidAppear() {
+        if welcomeFlag {
+            let alert = NSAlert() // 创建提示框
+            alert.messageText = NSLocalizedString("Welcome", comment: "Welcome") // 消息
+            alert.alertStyle = NSAlert.Style.informational // 提示类型
+            alert.runModal() // 显示模态框
+            welcomeFlag = false // 停止之后的欢迎提示
+        }
     }
     
     override var representedObject: Any? {
